0995c62fd300c141d908e6fe30b3c5bea162fdde
Rinatka777 <rinatsharifullin@gmail.com>
2025-05-23 21:47:32 +0300

    scraper.py with create dir and get all urls. download images unfinished

--- scraper.py
+++ scraper.py
@@ -3,43 +3,36 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-def create_dir(path):
+def create_dir(path):# create a path for a file 
     try:
         if not os.path.exists(path):
             os.makedirs(path)
     except OSError:
         print("Error")
 
-#def create_file(path):
-
 def get_all_urls(main_url):
-    # 1. Fetch the pages HTML
+    #Fetch the pages HTML
     resp = requests.get(main_url)
-    #      – Sends an HTTP GET request to the given URL.  
-    #      – Returns a Response object containing status code, headers, and content.  
-
+    #if ok, soup with html parser
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.content, "html.parser")
     else:
        print("Error")
        exit()
 
-    # 3. Select only the chapter links
+    #pick only ul ul li a 
     anchors = soup.select("div#Chapters_List ul ul li a")
-    #    • soup.select(css_selector)  
-    #      – Finds all elements matching the CSS selector you provide.  
-    #      – Here, "div#Chapters_List ul ul li a" drills down into the nested <ul>s to grab each <a>.  
 
-    # 4. Normalize URLs (make them absolute)
-    urls = []  # 1. Start with an empty list
-    for a in anchors:  # 2. Loop over each <a> tag
-        raw_href = a["href"]                # 3. Extract the href attribute
-        full_url = urljoin(main_url, raw_href)  # 4. Make it absolute
+    #Normalize URLs (make them absolute)
+    urls = []
+    for a in anchors:
+        raw_href = a["href"]
+        full_url = urljoin(main_url, raw_href)
         print(raw_href,"->", full_url)
-        urls.append(full_url)               # 5. Add it to the list
+        urls.append(full_url)
     
     return urls
 
 def download_images(img_urls, folder):
     create_dir(folder)
-    
+
